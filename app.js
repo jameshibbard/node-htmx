504c50d730daa6116343bef97a9c30bf25cfc196
James Hibbard <1940994+jameshibbard@users.noreply.github.com>
2024-03-11 12:37:52 +0100

    Remove body-parser

--- app.js
+++ app.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const path = require('path');
-const bodyParser = require('body-parser');
 const methodOverride = require('method-override');
 const routes = require('./routes/index');
 
@@ -10,7 +9,7 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
 app.use(methodOverride('_method'));
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
 app.use('/', routes);
 
